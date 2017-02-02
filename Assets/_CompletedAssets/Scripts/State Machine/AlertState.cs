7e537f81798145c9a730c627d57dd0a0264b765d
Nicholas Arnaud <cobrahead09@gmail.com>
2017-02-01 18:03:51 -0600

    Another Repo

---
+++ Assets/_CompletedAssets/Scripts/State Machine/AlertState.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AlertState : IEnemyState
+{
+    private readonly StatePatternEnemy enemy;
+    private float searchTimer;
+
+
+    public AlertState(StatePatternEnemy statepatternenemy)
+    {
+        enemy = statepatternenemy;
+    }
+
+    private void Look()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(enemy.eyes.transform.position, enemy.eyes.transform.forward, out hit, enemy.sightRange) && hit.collider.CompareTag("Player"))
+        {
+            enemy.chaseTarget = hit.transform;
+            ToChaseState();
+        }
+    }
+
+    public void ToChaseState()
+    {
+        enemy.currentState = enemy.chaseState;
+        searchTimer = 0f;
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+      
+    }
+
+    public void ToAlertState()
+    {
+        Debug.Log("Can't transition to the same state");
+    }
+
+    public void ToPatrolState()
+    {
+        enemy.currentState = enemy.patrolState;
+        searchTimer = 0f;
+    }
+
+    private void Search()
+    {
+        enemy.meshRendererFlag.material.color = Color.yellow;
+        enemy.navMeshAgent.Stop();
+        enemy.transform.Rotate(0, enemy.searchingTurnSpeed * Time.deltaTime, 0);
+        searchTimer += Time.deltaTime;
+        if (searchTimer >= enemy.searchingDuration)
+        {
+            ToPatrolState();
+        }
+    }
+
+    void IEnemyState.UpdateState()
+    {
+        Look();
+        Search();
+    }
+
+}
---
+++ Assets/_CompletedAssets/Scripts/State Machine/ChaseState.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChaseState : IEnemyState
+{
+    private readonly StatePatternEnemy enemy;
+
+
+    public ChaseState(StatePatternEnemy statepatternenemy)
+    {
+        enemy = statepatternenemy;
+    }
+
+    private void Look()
+    {
+        RaycastHit hit;
+        Vector3 enemyToTarget = (enemy.chaseTarget.position + enemy.offset)-enemy.eyes.transform.position;
+        if (Physics.Raycast(enemy.eyes.transform.position, enemyToTarget, out hit, enemy.sightRange) && hit.collider.CompareTag("Player"))
+            enemy.chaseTarget = hit.transform;
+        else
+            ToAlertState();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+       
+    }
+
+    public void ToAlertState()
+    {
+        enemy.currentState = enemy.alertState;
+    }
+
+    public void ToChaseState()
+    {
+       Debug.Log("Can't transition to the same state");
+    }
+
+    public void ToPatrolState()
+    {
+      Debug.Log("Can't transition to this state");
+    }
+
+    private void Chase()
+    {
+        enemy.meshRendererFlag.material.color = Color.red;
+        enemy.navMeshAgent.destination = enemy.chaseTarget.position;
+        enemy.navMeshAgent.Resume();
+
+    }
+
+
+    void IEnemyState.UpdateState()
+    {
+        Look();
+        Chase();
+    }
+
+}
